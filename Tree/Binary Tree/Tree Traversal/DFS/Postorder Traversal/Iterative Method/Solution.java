cae14d0d8c92989124d21bf47c574d53c028afe6
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-05-12 21:32:15 +0530

    Update Solution.java

--- Tree/Binary Tree/Tree Traversal/DFS/Postorder Traversal/Iterative Method/Solution.java
+++ Tree/Binary Tree/Tree Traversal/DFS/Postorder Traversal/Iterative Method/Solution.java
@@ -1,1 +1,33 @@
+class Solution
+{    
+    //Left
+    //Right
+    //Root
+    ArrayList<Integer> postOrder(Node root)
+    {
+      ArrayList<Integer>ans = new ArrayList<>();
+      if(root==null)
+      {
+          return ans;
+      }
+      Stack<Node>stack = new Stack<>();
+      stack.add(root);
+      while(!stack.isEmpty())
+      {
+          Node node=stack.pop();
+          ans.add(node.data);
+          if(node.left!=null)
+          {
+              stack.add(node.left);
+              
+          }
+          if(node.right!=null)
+          {
+              stack.add(node.right);
+          }
+      }
+      Collections.reverse(ans);
+      return ans;
+    }     
+}
 
