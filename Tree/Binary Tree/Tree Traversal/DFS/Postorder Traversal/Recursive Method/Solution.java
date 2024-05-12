eeb7c7c6b9f90a193f7c5ba3cac0eb9ab0972135
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-05-12 20:08:19 +0530

    Update Solution.java

--- Tree/Binary Tree/Tree Traversal/DFS/Postorder Traversal/Recursive Method/Solution.java
+++ Tree/Binary Tree/Tree Traversal/DFS/Postorder Traversal/Recursive Method/Solution.java
@@ -1,1 +1,19 @@
-
+class Solution
+{
+    ArrayList<Integer> postOrder(Node root)
+    {
+        ArrayList<Integer>ans=new ArrayList<>();
+        postOrderTraversal(root,ans);
+        return ans;
+    }
+    public void postOrderTraversal(Node root,ArrayList<Integer>ans)
+    {
+       if(root==null) 
+       {
+           return;
+       }
+       postOrderTraversal(root.left,ans);
+       postOrderTraversal(root.right,ans);
+       ans.add(root.data);
+    }
+}
