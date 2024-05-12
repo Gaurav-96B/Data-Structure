0bb3129d4176ea7fec23a13f8f9118c577adf789
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-05-12 22:57:33 +0530

    Update Solution.java

--- Tree/Binary Tree/Tree Traversal/BFS/Level Order Traversal/Iterative Method/Solution.java
+++ Tree/Binary Tree/Tree Traversal/BFS/Level Order Traversal/Iterative Method/Solution.java
@@ -1,1 +1,62 @@
+//First Approach
+class Solution
+{
+    static ArrayList <Integer> levelOrder(Node root) 
+    {
+        ArrayList<Integer>ans=new ArrayList<>();
+        if(root==null)
+        {
+            return ans;
+        }
+        Queue<Node>queue=new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty())
+        {
+           int size=queue.size();
+           for(int i=0;i<size;i++)
+           {
+            Node temp=queue.poll();
+            ans.add(temp.data);
+            if(temp.left!=null)
+            {
+                queue.add(temp.left);
+            }
+            if(temp.right!=null)
+            {
+                queue.add(temp.right);
+            }
+           }
+        }
+        return ans;
+    }
+}
+
+//Second Approach
+class Solution
+{
+    static ArrayList <Integer> levelOrder(Node root) 
+    {
+        ArrayList<Integer>ans=new ArrayList<Integer>();
+        if(root==null)
+        {
+            return ans;
+        }
+        Queue<Node>queue=new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty())
+        {
+            Node temp=queue.poll();
+            ans.add(temp.data);
+            if(temp.left!=null)
+            {
+                queue.add(temp.left);
+            }
+            if(temp.right!=null)
+            {
+                queue.add(temp.right);
+            }
+        }
+        return ans;
+    }
+}
 
