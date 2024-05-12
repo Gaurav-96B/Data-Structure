d8469ec090ef858a9afae9a5933f07c3a1225a49
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-05-12 19:59:15 +0530

    Update Solution.java

--- Tree/Binary Tree/Tree Traversal/DFS/Inorder Traversal/Iterative Method/Solution.java
+++ Tree/Binary Tree/Tree Traversal/DFS/Inorder Traversal/Iterative Method/Solution.java
@@ -1,1 +1,52 @@
-
+//First Approach
+class Solution {
+    // Function to return a list containing the inorder traversal of the tree.
+    ArrayList<Integer> inOrder(Node root) {
+        ArrayList<Integer>ans = new ArrayList<>();
+        Stack<Node>stack = new Stack<>();
+        Node node = root;
+        while(true)
+        {
+            if(node!=null)
+            {
+                stack.push(node);
+                node= node.left;
+            }
+            else
+            {
+               if(stack.isEmpty())
+               {
+                   break;
+               }
+               node = stack.pop();
+               ans.add(node.data);
+               node= node.right;
+            }
+        }
+        return ans;
+    }
+}
+//Second Approach
+class Solution {
+    // Function to return a list containing the inorder traversal of the tree.
+    ArrayList<Integer> inOrder(Node root) {
+        ArrayList<Integer>ans = new ArrayList<>();
+        Stack<Node>stack = new Stack<>();
+        Node node = root;
+        while(node!=null||!stack.isEmpty())
+        {
+            if(node!=null)
+            {
+                stack.push(node);
+                node= node.left;
+            }
+            else
+            {
+               node = stack.pop();
+               ans.add(node.data);
+               node= node.right;
+            }
+        }
+        return ans;
+    }
+}
