d8f8986cf4639a84541761b6845fe655fadc7d3f
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-05-12 20:00:20 +0530

    Update Solution.java

--- Tree/Binary Tree/Tree Traversal/DFS/Inorder Traversal/Recursive Method/Solution.java
+++ Tree/Binary Tree/Tree Traversal/DFS/Inorder Traversal/Recursive Method/Solution.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    // Function to return a list containing the inorder traversal of the tree.
+    ArrayList<Integer> inOrder(Node root) {
+        ArrayList<Integer>ans = new ArrayList<>();
+        inOrderTraversal(root,ans);
+        return ans;
+    }
+    void inOrderTraversal(Node root,ArrayList<Integer>ans)
+    {
+        if(root==null)
+        {
+            return;
+        }
+        inOrderTraversal(root.left,ans);
+        ans.add(root.data);
+        inOrderTraversal(root.right,ans);
+    }
+}
