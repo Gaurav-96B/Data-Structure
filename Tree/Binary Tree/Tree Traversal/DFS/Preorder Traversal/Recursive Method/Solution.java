f1a8e19880f4652a2c8e72d0546e4aefd3328af6
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-05-12 19:23:42 +0530

    Update Solution.java

--- Tree/Binary Tree/Tree Traversal/DFS/Preorder Traversal/Recursive Method/Solution.java
+++ Tree/Binary Tree/Tree Traversal/DFS/Preorder Traversal/Recursive Method/Solution.java
@@ -1,1 +1,23 @@
-
+class Solution
+{
+    //Function to return a list containing the preorder traversal of the tree.
+    static ArrayList<Integer> preorder(Node root)
+    {
+      ArrayList<Integer>ans = new ArrayList<>();
+      preOrder(root,ans);
+      return ans;
+    }
+    
+    static void preOrder(Node node, ArrayList<Integer>ans)
+    {
+        if(node==null)
+        {
+           return; 
+        }
+        ans.add(node.data);
+        preOrder(node.left,ans);
+        preOrder(node.right,ans);
+        return;
+    }
+  
+}
