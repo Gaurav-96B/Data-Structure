5977da0711595a6fa0478eaa7b41f26f0222a865
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-05-12 21:04:26 +0530

    Update Solution.java

--- Tree/Binary Tree/Tree Traversal/DFS/Preorder Traversal/Morris Traversal/Solution.java
+++ Tree/Binary Tree/Tree Traversal/DFS/Preorder Traversal/Morris Traversal/Solution.java
@@ -1,1 +1,47 @@
-
+class Solution
+{    
+    //Root
+    //Left
+    //Right
+    static ArrayList<Integer> preorder(Node root)
+    {
+        ArrayList<Integer>ans=new ArrayList<>();
+        if(root==null)
+        {
+            return ans;
+        }
+        while(root!=null)
+        {
+            //if left part does not exist
+            if(root.left==null)
+            {
+                ans.add(root.data);
+                root=root.right;
+            }
+            //if left part exist
+            else
+            {
+              Node curr=root.left;
+              while(curr.right!=null&&curr.right!=root)
+              {
+                  curr=curr.right;
+              }
+              //If left Subtree Not Traverse
+              if(curr.right==null)
+              {
+                  ans.add(root.data);
+                  curr.right=root;
+                  root=root.left;
+              }
+              //If left Subtree Traverse
+              else
+              {
+                  curr.right=null;
+                  root=root.right;
+              }
+            }
+            
+        }
+        return ans;
+    }
+}
