6549996acec39972c93a7dc3cb764be60b4c2367
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-05-12 23:02:59 +0530

    Update Solution.java

--- Tree/Binary Tree/Tree Traversal/BFS/Level Order Traversal/Recursive Method/Solution.java
+++ Tree/Binary Tree/Tree Traversal/BFS/Level Order Traversal/Recursive Method/Solution.java
@@ -1,1 +1,39 @@
+class Solution
+{
+    static ArrayList <Integer> levelOrder(Node node) 
+    {
+       ArrayList<Integer>ans=new ArrayList<>();
+       if(node==null)
+       {
+           return ans;
+       }
+       int height=findHeight(node);
+       for(int i=0;i<=height;i++)
+       {
+           printAtEachLevel(node,i+1,ans);
+       }
+       return ans;
+    }
+    public static int findHeight(Node node)
+    {
+       if(node==null) 
+       {
+         return 0;  
+       }
+       return 1+Math.max(findHeight(node.left),findHeight(node.right));
+    }
+    public static void printAtEachLevel(Node node,int level,ArrayList<Integer>ans)
+    {
+        if(node==null)
+        {
+            return;
+        }
+        if(level==1)
+        {
+            ans.add(node.data);
+        }
+        printAtEachLevel(node.left,level-1,ans);
+        printAtEachLevel(node.right,level-1,ans);
+    }
+}
 
