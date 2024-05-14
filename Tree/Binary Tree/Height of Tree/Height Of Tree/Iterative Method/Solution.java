2a883fd79dc4fb92778af08aaab094a0f709b958
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-05-14 12:22:28 +0530

    Update Solution.java

--- Tree/Binary Tree/Height of Tree/Height Of Tree/Iterative Method/Solution.java
+++ Tree/Binary Tree/Height of Tree/Height Of Tree/Iterative Method/Solution.java
@@ -1,1 +1,34 @@
+class Solution {
+    int height(Node node) 
+    {
+       if(node==null)
+       {
+           return 0;
+       }
+       int height=0;
+       Queue<Node>queue=new LinkedList<>();
+       queue.add(node);
+       while(!queue.isEmpty())
+       {
+           int size=queue.size();
+           if(size!=0)
+           {
+              height++; 
+           }
+           for(int i=0;i<size;i++)
+           {
+               Node temp=queue.poll();
+               if(temp.left!=null)
+               {
+                   queue.add(temp.left);
+               }
+               if(temp.right!=null)
+               {
+                   queue.add(temp.right);
+               }
+           }
+       }
+       return height;
+    }
+}
 
