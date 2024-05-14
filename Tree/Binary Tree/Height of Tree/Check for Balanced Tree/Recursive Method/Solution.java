8cea6ce30b97a635104b172cdb765a4470af15ea
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-05-14 12:28:03 +0530

    Create Solution.java

---
+++ Tree/Binary Tree/Height of Tree/Check for Balanced Tree/Recursive Method/Solution.java
@@ -0,0 +1,37 @@
+class Solution
+{
+    boolean isBalanced(Node root)
+    {
+     if(height(root)>0){
+     return true;
+     }
+     else{
+     return false;
+     }
+    }
+    int height(Node root)
+    {
+        if(root==null)
+        {
+            return 0;
+        }
+        int leftHeight=height(root.left);
+        if(leftHeight==-1)
+        {
+            return -1;
+        }
+        int righHeight=height(root.right);
+        if(righHeight==-1)
+        {
+            return -1;
+        }
+        if(Math.abs(leftHeight-righHeight)>1)
+        {
+            return -1;
+        }
+        else
+        {
+        return 1+Math.max(leftHeight,righHeight);
+        }
+    }
+}
