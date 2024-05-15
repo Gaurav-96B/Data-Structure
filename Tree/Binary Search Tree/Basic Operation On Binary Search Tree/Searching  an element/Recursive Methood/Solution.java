1184c05646638af31189d7a04a463119f518bab2
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-05-15 11:13:01 +0530

    Update Solution.java

--- Tree/Binary Search Tree/Basic Operation On Binary Search Tree/Searching  an element/Recursive Methood/Solution.java
+++ Tree/Binary Search Tree/Basic Operation On Binary Search Tree/Searching  an element/Recursive Methood/Solution.java
@@ -1,1 +1,21 @@
+class Solution {
+    boolean search(Node root, int x) {
+        if(root==null)
+        {
+            return false;
+        }
+        if(root.data==x)
+        {
+            return true;
+        }
+        if(x<root.data)
+        {
+          return search(root.left,x);
+        }
+        else
+        {
+          return search(root.right,x);
+        }
+    }
+}
 
