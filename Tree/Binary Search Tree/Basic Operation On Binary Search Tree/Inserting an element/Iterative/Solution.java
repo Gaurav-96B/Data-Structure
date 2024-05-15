39c1066fd1fc6c412af29862cd3f49b28e70c36e
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-05-15 11:05:50 +0530

    Update Solution.java

--- Tree/Binary Search Tree/Basic Operation On Binary Search Tree/Inserting an element/Iterative/Solution.java
+++ Tree/Binary Search Tree/Basic Operation On Binary Search Tree/Inserting an element/Iterative/Solution.java
@@ -1,18 +1,1 @@
-class Solution {
-    Node insert(Node root, int Key) {
-        if(root==null)
-        {
-          root=new Node(Key); 
-          return root;
-        }
-        if(Key<root.data)
-        {
-            root.left=insert(root.left,Key);
-        }
-        if(Key>root.data)
-        {
-            root.right=insert(root.right,Key);
-        }
-        return root;
-    }
-}
+
