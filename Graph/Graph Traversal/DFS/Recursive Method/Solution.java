18a3a6bcaa6be2e363fa1a99a38b59f04cdae325
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-06-01 17:58:18 +0530

    Update Solution.java

--- Graph/Graph Traversal/DFS/Recursive Method/Solution.java
+++ Graph/Graph Traversal/DFS/Recursive Method/Solution.java
@@ -1,1 +1,56 @@
+                                             Method-1
+-----------------------------------------------------------------------------------------------------------------------   
+class Solution {
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer>ans=new ArrayList<>();
+        boolean visited[]=new boolean[V];
+        dfs(adj,0,visited,ans);
+        return ans;
+    }
+    public void dfs(ArrayList<ArrayList<Integer>> adj,int src,boolean visited[],ArrayList<Integer>ans)
+    {
+        visited[src]=true;
+        ans.add(src);
+        for(int adjacent:adj.get(src))
+        {
+            if(visited[adjacent]==false)
+            {
+                dfs(adj,adjacent,visited,ans);
+            }
+        }
+        
+    }
+    
+}
+                                     Method-2
+-----------------------------------------------------------------------------------------------------------------------------
+
+class Solution {
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer>ans=new ArrayList<>();
+        boolean visited[]=new boolean[V];
+        for(int i=0;i<V;i++)
+        {
+            if(visited[i]==false)
+            {
+                dfs(adj,i,visited,ans);
+            }
+        }
+        return ans;
+    }
+    public void dfs(ArrayList<ArrayList<Integer>> adj,int src,boolean visited[],ArrayList<Integer>ans)
+    {
+        visited[src]=true;
+        ans.add(src);
+        for(int adjacent:adj.get(src))
+        {
+            if(visited[adjacent]==false)
+            {
+                dfs(adj,adjacent,visited,ans);
+            }
+        }
+        
+    }
+}
+
 
