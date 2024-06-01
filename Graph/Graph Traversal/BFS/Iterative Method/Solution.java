1c4165bc59ce1a6df810e9220a693c5dbd142548
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-06-01 18:12:32 +0530

    Update Solution.java

--- Graph/Graph Traversal/BFS/Iterative Method/Solution.java
+++ Graph/Graph Traversal/BFS/Iterative Method/Solution.java
@@ -1,1 +1,24 @@
-
+class Solution {
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer>ans=new ArrayList<>();
+        Queue<Integer>queue=new LinkedList<>();
+        boolean visited[]=new boolean[V];
+        queue.add(0);
+        visited[0]=true;
+        while(!queue.isEmpty())
+        {
+            int temp=queue.poll();
+            ans.add(temp);
+            for(int adjacent:adj.get(temp))
+            {
+                if(visited[adjacent]==false)
+                {
+                    queue.add(adjacent);
+                    visited[adjacent]=true;
+                }
+                
+            }
+        }
+        return ans;
+    }
+}
