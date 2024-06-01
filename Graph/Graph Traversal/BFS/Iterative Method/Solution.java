546eb8c3107d9b8e62720310015c39cba4d6b2cf
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-06-01 21:08:50 +0530

    Update Solution.java

--- Graph/Graph Traversal/BFS/Iterative Method/Solution.java
+++ Graph/Graph Traversal/BFS/Iterative Method/Solution.java
@@ -3,22 +3,28 @@ class Solution {
         ArrayList<Integer>ans=new ArrayList<>();
         Queue<Integer>queue=new LinkedList<>();
         boolean visited[]=new boolean[V];
-        queue.add(0);
-        visited[0]=true;
+        bfs(adj,0,queue,visited,ans);
+        return ans;
+        
+    }
+    public void bfs(ArrayList<ArrayList<Integer>> adj,int src,Queue<Integer>queue,boolean visited[],ArrayList<Integer>ans)
+    {
+        queue.add(src);
+        visited[src]=true;
         while(!queue.isEmpty())
         {
-            int temp=queue.poll();
-            ans.add(temp);
-            for(int adjacent:adj.get(temp))
+            int data=queue.peek();
+            queue.poll();
+            ans.add(data);
+            for(int adjacent:adj.get(data))
             {
-                if(visited[adjacent]==false)
-                {
-                    queue.add(adjacent);
-                    visited[adjacent]=true;
-                }
-                
+              if(visited[adjacent]==false) 
+              {
+                  visited[adjacent]=true;
+                  queue.add(adjacent);
+              }
             }
         }
-        return ans;
     }
+    
 }
