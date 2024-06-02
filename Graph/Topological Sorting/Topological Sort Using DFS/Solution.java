0b0fc30e8553c2389231544503fc9adc72fc0ec1
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-06-02 16:44:23 +0530

    Update Solution.java

--- Graph/Topological Sorting/Topological Sort Using DFS/Solution.java
+++ Graph/Topological Sorting/Topological Sort Using DFS/Solution.java
@@ -1,1 +1,38 @@
-
+class Solution
+{
+    static int[] topoSort(int N, ArrayList<ArrayList<Integer>> adj) 
+    {
+      boolean visited[]=new boolean[N];
+      Stack<Integer>stack=new Stack<>();
+      for(int i=0;i<N;i++)
+      {
+          if(visited[i]==false)
+          {
+              topologicalSortUsingDfs(adj,i,visited,stack);
+          }
+      }
+      
+      int ans[]=new int[N];
+      int k=0;
+      while(!stack.isEmpty())
+      {
+         ans[k++]=stack.pop();
+      }
+      return ans;
+        
+    }
+  
+    static void topologicalSortUsingDfs(ArrayList<ArrayList<Integer>> adj,int src,boolean visited[],Stack<Integer>stack)
+    {
+        visited[src]=true;
+        for(int adjacent:adj.get(src))
+        {
+            if(visited[adjacent]==false)
+            {
+               topologicalSortUsingDfs(adj,adjacent,visited,stack);
+            }
+        }
+        stack.push(src);
+    }
+    
+}
