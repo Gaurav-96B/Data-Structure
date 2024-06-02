716a266e9649d8e6292702ea8549942edf01a5e6
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-06-02 16:36:13 +0530

    Update Solution.java

--- Graph/Topological Sorting/Topological Sorti Using BFS/Kahn’s Algorithm/Solution.java
+++ Graph/Topological Sorting/Topological Sorti Using BFS/Kahn’s Algorithm/Solution.java
@@ -1,1 +1,38 @@
-
+class Solution
+{
+    static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
+    {
+        int ans[] = new int[V];
+       
+      //Find the indegree of all vertex
+        int indegree[] = new int[V]; 
+        for(int i = 0;i<V;i++) {
+            for(int adjacent: adj.get(i)) {
+                indegree[adjacent]++; 
+            }
+        }
+        
+      //Push zero indegree vertex into queue
+       Queue<Integer> q = new LinkedList<Integer>(); 
+        for(int i = 0;i<V;i++) {
+            if(indegree[i] == 0) {
+                q.add(i); 
+            }
+        }
+        
+      //Pop the element from queue while queue is not empty
+       int index=0;
+       while(!q.isEmpty()) {
+            int node=q.poll(); 
+            ans[index++]=node;
+            for(int adjacent:adj.get(node)) {
+                indegree[adjacent]--; 
+                if(indegree[adjacent] == 0) {
+                    q.add(adjacent); 
+                }
+            }
+        }
+        return ans;
+    }
+    
+}
