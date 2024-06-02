48060fe2f3861ede3a8b4b0accfe2b660abb3723
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-06-02 12:36:55 +0530

    Update Solution.java

--- Graph/Cycles in Graph/Undirected Graph/Detect cycle using DFS/Solution.java
+++ Graph/Cycles in Graph/Undirected Graph/Detect cycle using DFS/Solution.java
@@ -1,1 +1,28 @@
-
+class Solution {
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean visited[]=new boolean[V];
+        for(int i=0;i<V;i++)
+        {
+            if(visited[i]==false)
+            {
+            if(detectCycleUsingDfs(adj,i,-1,visited))
+            return true;
+            }
+        }
+        return false;
+    }
+    public boolean detectCycleUsingDfs(ArrayList<ArrayList<Integer>> adj,int src,int parent,boolean visited[])
+    {
+        visited[src]=true;
+        for(int adjacent:adj.get(src))
+        {
+            if(visited[adjacent]==false)
+            {
+                if(detectCycleUsingDfs(adj,adjacent,src,visited))
+                return true;
+            }
+            else if(adjacent!=parent)
+            return true;
+        }
+        return false;
+    }
