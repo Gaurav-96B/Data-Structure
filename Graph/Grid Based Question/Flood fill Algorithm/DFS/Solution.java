fa33e403b34c1ce3419c73b1a99077307cc0da56
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-06-08 19:20:05 +0530

    Update Solution.java

--- Graph/Grid Based Question/Flood fill Algorithm/DFS/Solution.java
+++ Graph/Grid Based Question/Flood fill Algorithm/DFS/Solution.java
@@ -1,1 +1,22 @@
-
+//Number of island application
+class Solution
+{
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor){
+        
+	       dfs(image,sr,sc,newColor,image[sr][sc]);
+	       return image;
+    }
+    
+    public void dfs(int image[][],int x,int y,int newcolor,int oldcolor)
+        {
+            if(x<0 || y<0 || x>=image.length || y>=image[0].length || image[x][y]==newcolor ||image[x][y]!=oldcolor)
+            {
+                return;
+            }
+            image[x][y]=newcolor;
+            dfs(image,x-1,y,newcolor,oldcolor);
+            dfs(image,x+1,y,newcolor,oldcolor);
+            dfs(image,x,y-1,newcolor,oldcolor);
+            dfs(image,x,y+1,newcolor,oldcolor);
+        }
+}
