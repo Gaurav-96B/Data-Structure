67932c7395863265d715bcc91340e50d1e318517
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-06-12 18:59:22 +0530

    Create Solution.java

---
+++ Linked List/Two Pointer Algorithm/Floyd's Tortoise and Hare Algorithm/Finding middle element in a linked list/Solution.java
@@ -0,0 +1,18 @@
+/*TC=>O(n/2)
+SC=>O(1)*/
+
+class Solution
+{
+    int getMiddle(Node head){
+        if(head==null){
+           return 0; 
+        }
+        Node slow=head;
+        Node fast=head;
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow.data;
+    }
+}
