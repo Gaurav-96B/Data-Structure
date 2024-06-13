eb92520a7d63ba5368dda0d68b713c5c0b92cd79
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-06-13 11:16:53 +0530

    Update Solution.java

--- Linked List/Reverse Linked List Problems/Reverse a linked list/Recursive Method/Solution.java
+++ Linked List/Reverse Linked List Problems/Reverse a linked list/Recursive Method/Solution.java
@@ -1,1 +1,15 @@
-
+class Solution
+{
+    //Function to reverse a linked list.
+    Node reverseList(Node head)
+    {
+       if(head==null||head.next==null){
+           return head;
+       }
+       Node newHead=reverseList(head.next);
+       Node front=head.next;
+       front.next=head;
+       head.next=null;
+       return newHead;
+    }
+}
