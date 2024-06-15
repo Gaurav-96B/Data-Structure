39ce1218dcb55ae28b59740fd1ec197a371bad5d
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-06-15 20:47:02 +0530

    Update Solution.java

--- Linked List/Arrangement of Linked List/Rearrange a linked list/Solution.java
+++ Linked List/Arrangement of Linked List/Rearrange a linked list/Solution.java
@@ -1,1 +1,41 @@
 
+class Solution
+{
+
+   // Should rearrange given linked list such that all even
+   // positioned Nodes are before odd positioned.
+   // Returns nothing
+    void rearrangeEvenOdd(Node head)
+    {
+          if(head==null||head.next==null){
+              return;
+          }
+          
+          Node dummyEven=new Node(-1);
+          Node tempEven=dummyEven;
+          Node dummyOdd=new Node(-1);
+          Node tempOdd=dummyOdd;
+          
+          Node temp=head;
+          boolean flag=false;
+          while(temp!=null){
+              if(flag==true){
+                  tempEven.next=temp;
+                  tempEven=tempEven.next;
+                  temp=temp.next;
+                  flag=false;
+              }
+              else{
+                  tempOdd.next=temp;
+                  tempOdd=tempOdd.next;
+                  temp=temp.next;
+                  flag=true;
+              }
+          }
+          tempEven.next=null;
+          tempOdd.next=null;
+          tempOdd.next=dummyEven.next;
+
+          
+     }
+}
