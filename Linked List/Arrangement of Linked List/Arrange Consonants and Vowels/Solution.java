2a7e235f150d8a1569ce276e627546071cc7af5d
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-06-15 20:52:57 +0530

    Update Solution.java

--- Linked List/Arrangement of Linked List/Arrange Consonants and Vowels/Solution.java
+++ Linked List/Arrangement of Linked List/Arrange Consonants and Vowels/Solution.java
@@ -1,1 +1,30 @@
+class Solution {
 
+    public Node arrangeCV(Node head){
+        Node dummyConsonants =new Node(Character.MIN_VALUE);
+        Node tempConsonants=dummyConsonants;
+        Node dummyVowel=new Node(Character.MIN_VALUE);
+        Node tempVowel=dummyVowel;
+        
+        Node temp=head;
+        while(temp!=null)
+        {
+            if(temp.data=='a'|| temp.data=='e' || temp.data=='i' || temp.data=='o' || temp.data=='u'){
+               tempVowel.next=temp;
+               tempVowel=tempVowel.next;
+               temp=temp.next;
+               
+            }
+            else{
+                tempConsonants.next=temp;
+                tempConsonants=tempConsonants.next;
+                temp=temp.next;
+            }
+        }
+        tempVowel.next=null;
+        tempConsonants.next=null;
+        tempVowel.next=dummyConsonants.next;
+        return dummyVowel.next;
+        
+    }
+}
