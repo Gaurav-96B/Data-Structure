d03f76a657d52d3ab5344cce04948c11927cee15
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-06-16 12:11:13 +0530

    Update Solution.java

--- Linked List/Reverse Linked List Problems/Reverse a linked list/Iterative Method/Solution.java
+++ Linked List/Reverse Linked List Problems/Reverse a linked list/Iterative Method/Solution.java
@@ -3,18 +3,19 @@ class Solution
     //Function to reverse a linked list.
     Node reverseList(Node head)
     {
-        if(head==null || head.next==null){
-            return head;
-        }
-        Node prev=null;
-        Node temp=head;
-        while(temp!=null){
-            Node front=temp.next;
-            temp.next=prev;
-            prev=temp;
-            temp=front;
-        }
-        head=prev;
-        return head;
+      if(head==null||head.next==null){
+          return head;
+      }
+      Node prev=null;
+      Node curr=head;
+      Node front=head;
+      while(curr!=null){
+          front=curr.next;
+          curr.next=prev;
+          prev=curr;
+          curr=front;
+      }
+      head=prev;
+      return head;
     }
 }
