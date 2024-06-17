83129d4504dc1027490368367d722c4b842e0ba4
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-06-17 18:33:34 +0530

    Update Solution.java

--- Algorithms/Two Pointers Technique/Binary Array Sorting/Solution.java
+++ Algorithms/Two Pointers Technique/Binary Array Sorting/Solution.java
@@ -1,1 +1,26 @@
+class Solution
+{
+    //Function to sort the binary array.
+    static void binSort(int A[], int N)
+    {
+        int start=0;
+        int end=N-1;
+        while(start<end){
+            if(A[start]>A[end]){
+                int temp=A[start];
+                A[start]=A[end];
+                A[end]=temp;
+                start++;
+                end--;
+            }
+            else if(A[start]==0){
+                start++;
+            }
+            else if(A[end]==1){
+                end--;
+            }
+    }
+  }
+}
+
 
