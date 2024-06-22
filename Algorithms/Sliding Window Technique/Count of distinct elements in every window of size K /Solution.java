5b0100ee88d81a44716482b20363bff9cf2d97d1
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-06-22 13:59:39 +0530

    Update Solution.java

--- Algorithms/Sliding Window Technique/Count of distinct elements in every window of size K /Solution.java
+++ Algorithms/Sliding Window Technique/Count of distinct elements in every window of size K /Solution.java
@@ -1,1 +1,26 @@
-
+class Solution
+{
+    ArrayList<Integer> countDistinct(int A[], int n, int k)
+    {
+        ArrayList<Integer>ans=new ArrayList<>();
+        Map<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            if(map.containsKey(A[i])==false){
+                map.put(A[i],1);
+            }
+            else{
+                map.put(A[i],map.get(A[i])+1);
+            }
+            if(i>=k-1){
+                ans.add(map.size());
+                if(map.get(A[i-(k-1)])==1){
+                    map.remove(A[i-(k-1)]);
+                }
+                else{
+                    map.put(A[i-(k-1)],map.get(A[i-(k-1)])-1);
+                }
+            }
+        }
+        return ans;
+    }
+}
