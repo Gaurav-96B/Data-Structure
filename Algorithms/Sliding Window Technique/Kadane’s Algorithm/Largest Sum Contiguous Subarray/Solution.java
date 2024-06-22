e5975e5638474635e2d1622fe53063b76feb6c80
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-06-22 17:10:20 +0530

    Update Solution.java

--- Algorithms/Sliding Window Technique/Kadane’s Algorithm/Largest Sum Contiguous Subarray/Solution.java
+++ Algorithms/Sliding Window Technique/Kadane’s Algorithm/Largest Sum Contiguous Subarray/Solution.java
@@ -1,1 +1,15 @@
-
+class Solution{
+    long maxSubarraySum(int arr[], int n){
+        long max=Long.MIN_VALUE;
+        long sum=0;
+        for(int i=0;i<n;i++) {
+            sum=sum+arr[i];
+            max=Math.max(max,sum);
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return max;
+    }
+    
+}
