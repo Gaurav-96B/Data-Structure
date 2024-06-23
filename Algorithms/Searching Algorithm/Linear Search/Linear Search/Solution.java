9cca378971bdc649e1cba06e7e88cf060d5e3e13
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-06-23 23:07:09 +0530

    Update Solution.java

--- Algorithms/Searching Algorithm/Linear Search/Linear Search/Solution.java
+++ Algorithms/Searching Algorithm/Linear Search/Linear Search/Solution.java
@@ -1,1 +1,11 @@
+class Solution {
 
+    static int search(int arr[], int n, int x) {
+        for(int i=0;i<n;i++){
+            if(arr[i]==x){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
