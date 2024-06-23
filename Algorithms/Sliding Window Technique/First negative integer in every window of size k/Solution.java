6720213a54f6cd936fa02e765f83693fe2c334ac
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-06-23 16:27:34 +0530

    Update Solution.java

--- Algorithms/Sliding Window Technique/First negative integer in every window of size k/Solution.java
+++ Algorithms/Sliding Window Technique/First negative integer in every window of size k/Solution.java
@@ -1,1 +1,30 @@
 
+class Solution {
+    
+    public long[] printFirstNegativeInteger(long a[], int n, int k)
+    {
+        Queue<Long>queue=new LinkedList<>();
+        long result[]=new long[n-k+1];
+        int idx=0;
+        for(int i=0;i<n;i++){
+            if(a[i]<0){
+                queue.add(a[i]);
+            }
+            if(i>=k-1){
+                if(queue.size()==0){
+                    result[idx++]=0;
+                }
+                else{
+                    if(a[i-(k-1)]==queue.peek()){
+                       result[idx++]=queue.poll();
+
+                    }
+                    else{
+                    result[idx++]=queue.peek();
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
