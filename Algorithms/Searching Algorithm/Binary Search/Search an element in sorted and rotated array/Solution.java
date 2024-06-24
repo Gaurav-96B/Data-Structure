915e91633f4a85a17b660267d707a4cddda58914
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-06-24 11:20:39 +0530

    Update Solution.java

--- Algorithms/Searching Algorithm/Binary Search/Search an element in sorted and rotated array/Solution.java
+++ Algorithms/Searching Algorithm/Binary Search/Search an element in sorted and rotated array/Solution.java
@@ -1,1 +1,34 @@
+class Solution 
+{ 
+    static int Search(int arr[], int target)
+	  {
+	      int start=0;
+        int mid=0;
+        int end=arr.length-1;
+        while(start<=end){
+            mid=start+(end-start)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            else if(arr[start]<=arr[mid]){
+                if(target>=arr[start]&&arr[mid]>target){
+                    end=mid-1;
+                }
+                else{
+                start=mid+1;
+                }
+            }
+            else if(arr[mid]<=arr[end]) {
+                if(target>arr[mid]&&arr[end]>=target){
+                    start=mid+1;
+                }
+                else{
+                end=mid-1;
+                }
+            }
+        }
+        return -1;
+	}
+} 
+
 
