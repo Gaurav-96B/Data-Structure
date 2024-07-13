8f7eba53cbda9a3643d0213a7a1e8f31c48dc0e6
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-07-13 18:44:40 +0530

    Update Solution.java

--- Algorithms/Sorting Algorithm/Find Second largest element in an array/Solution.java
+++ Algorithms/Sorting Algorithm/Find Second largest element in an array/Solution.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public int print2largest(List<Integer> arr) {
+        int firstLargest=arr.get(0);
+        int secondLargest=-1;
+        for(int i=1;i<arr.size();i++){
+            if(arr.get(i)>firstLargest){
+                secondLargest=firstLargest;
+                firstLargest=arr.get(i);
+            }
+            else if(arr.get(i)<firstLargest && arr.get(i)>secondLargest){
+                secondLargest=arr.get(i);
+            }
+        }
+        return secondLargest;
+    }
+}
