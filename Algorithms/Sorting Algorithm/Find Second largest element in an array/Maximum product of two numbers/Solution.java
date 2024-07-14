3f655682aa70f2b88fc5efb99db779de9b7e84d4
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-07-14 21:19:10 +0530

    Update Solution.java

--- Algorithms/Sorting Algorithm/Find Second largest element in an array/Maximum product of two numbers/Solution.java
+++ Algorithms/Sorting Algorithm/Find Second largest element in an array/Maximum product of two numbers/Solution.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    int maxProduct(int arr[], int n) {
+        int firstLargest=arr[0];
+        int secondLargest=-1;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>firstLargest){
+                secondLargest=firstLargest;
+                firstLargest=arr[i];
+            }
+            else if(arr[i]<=firstLargest && arr[i]>secondLargest){
+                secondLargest=arr[i];
+            }
+        }
+        return firstLargest*secondLargest;
+    }
+}
