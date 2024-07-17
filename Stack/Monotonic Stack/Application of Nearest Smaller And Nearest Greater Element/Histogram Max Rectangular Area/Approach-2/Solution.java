b97ad816f5aba034296420b496333cfbe09a34db
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-07-17 23:50:17 +0530

    Update Solution.java

--- Stack/Monotonic Stack/Application of Nearest Smaller And Nearest Greater Element/Histogram Max Rectangular Area/Approach-2/Solution.java
+++ Stack/Monotonic Stack/Application of Nearest Smaller And Nearest Greater Element/Histogram Max Rectangular Area/Approach-2/Solution.java
@@ -1,1 +1,20 @@
+class Solution
+{
+    public static long getMaxArea(long hist[], long n) 
+    {
+        Stack<Integer> s = new Stack<>();
+        long maxArea=0;
+        for (int i=0;i<=n; i++) {
+            long h=(i==n)?0:hist[i];
+            while (!s.isEmpty() && hist[s.peek()] > h) {
+                long ele=s.pop();
+                long nse=i;
+                long pse=s.isEmpty()? -1:s.peek();
+                maxArea=Math.max(hist[(int) ele]*(nse - pse - 1),maxArea);
+            }
+            s.push(i);
+        }
+        return maxArea;
+  }
+}
 
