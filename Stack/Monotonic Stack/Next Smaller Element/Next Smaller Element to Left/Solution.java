892498b081acab379bcb4f924bee0db8b7e6186a
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-07-17 19:27:24 +0530

    Update Solution.java

--- Stack/Monotonic Stack/Next Smaller Element/Next Smaller Element to Left/Solution.java
+++ Stack/Monotonic Stack/Next Smaller Element/Next Smaller Element to Left/Solution.java
@@ -1,1 +1,21 @@
+class Solution{
+    static List<Integer> leftSmaller(int n, int a[])
+    {
+        List<Integer>ans=new ArrayList<>();
+        Stack<Integer>stack=new Stack<>();
+        for(int i=0;i<n;i++){
+            while(!stack.isEmpty()&&stack.peek()>=a[i]){
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                ans.add(-1);
+            }
+            else{
+                ans.add(stack.peek());
+             }
+            stack.push(a[i]);
 
+    }
+    return ans;
+}
+}
