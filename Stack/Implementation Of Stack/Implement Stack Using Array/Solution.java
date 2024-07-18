e6dffe48ccc6056af65366eca30b2383c7d4a6bd
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-07-18 15:16:10 +0530

    Update Solution.java

--- Stack/Implementation Of Stack/Implement Stack Using Array/Solution.java
+++ Stack/Implementation Of Stack/Implement Stack Using Array/Solution.java
@@ -1,1 +1,70 @@
+public class Stack {
+    private int arr[];
+    private int size;
+    private int top;
 
+    public Stack(int size) {
+        this.size = size;
+        this.arr = new int[size];
+        this.top = -1;
+    }
+
+    public void push(int a) {
+        if (top == size - 1) {
+            System.out.println("Stack Overflow");
+        } else {
+            top = top + 1;
+            arr[top] = a;
+        }
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public void pop() {
+        if (top == -1) {
+            System.out.println("Stack Underflow");
+        } else {
+            top = top - 1;
+        }
+    }
+
+    public int size() {
+        return top + 1;
+    }
+
+    public int peek() {
+        return arr[top];
+    }
+
+    public static void main(String[] args) {
+        Stack stack = new Stack(6);
+        stack.push(5);
+        System.out.println("Current size of stack is: " + stack.size());
+        stack.push(10);
+        stack.push(15);
+        stack.push(20);
+        stack.push(25);
+        stack.push(30);
+        System.out.println("Current size of stack is: " + stack.size());
+
+        // Attempting to push into a full stack
+        stack.push(35);
+
+        // Accessing the top element
+        System.out.println("At present, the top element in stack is: " + stack.peek());
+
+        // Removing all the elements from the stack
+        for (int i = 0; i < 6; i++) {
+            stack.pop();
+        }
+        System.out.println("Current size of stack is: " + stack.size());
+
+        // Attempting to pop from an empty stack
+        stack.pop();
+
+        System.out.println("Is Stack Empty : " + stack.isEmpty());
+
+    }
+}
