63bfce21dbbc615e2843e2367a8ac49fbf5c1f72
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-07-18 16:44:37 +0530

    Update Solution.java

--- Stack/Implementation Of Stack/Implement Stack Using Queue/Implement Stack Using One Queue/Solution.java
+++ Stack/Implementation Of Stack/Implement Stack Using Queue/Implement Stack Using One Queue/Solution.java
@@ -1,1 +1,63 @@
+import java.util.*;
+class Stack {
+    Queue<Integer> q1 = new LinkedList<>();
 
+    public void push(int x) { 
+       //  Get previous size of queue
+        int s = q1.size();
+
+        // Push the current element
+        q1.add(x);
+
+        // Pop all the previous elements and put them after
+        // current element
+        for (int i = 0; i < s; i++) {
+            q1.add(q1.poll());
+        }
+        
+    }
+    
+    public boolean isEmpty() {
+        return q1.isEmpty();
+    }
+    
+    public void pop(){
+        // if no elements are there in q1
+        if (q1.isEmpty()){
+            return;
+        }
+        q1.remove();
+    }
+    
+    public int size() { 
+        return q1.size(); 
+        
+    }
+    
+    public int peek(){
+       if (q1.isEmpty()){
+            return -1;
+       }
+        return q1.peek();
+    }
+
+    public static void main(String[] args)
+    {
+        Stack stack = new Stack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        System.out.println("current size: " + stack.size());
+        System.out.println(stack.peek());
+        stack.pop();
+        System.out.println(stack.peek());
+        stack.pop();
+        System.out.println(stack.peek());
+        stack.pop();
+        System.out.println("current size: " + stack.size());
+        System.out.println(stack.isEmpty());
+    }
+}
+
+// This code is contributed by Princi Singh
