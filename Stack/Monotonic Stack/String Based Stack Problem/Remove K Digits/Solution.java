0afd2af22c94bc2f5ffacd80c2ab6646ad218ade
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-07-18 12:48:04 +0530

    Update Solution.java

--- Stack/Monotonic Stack/String Based Stack Problem/Remove K Digits/Solution.java
+++ Stack/Monotonic Stack/String Based Stack Problem/Remove K Digits/Solution.java
@@ -1,1 +1,31 @@
-
+class Solution {
+    public String removeKdigits(String S, int K) {
+        Stack<Character> stack=new Stack<>();
+        for(int i=0;i<S.length();i++){
+            while(!stack.isEmpty() && stack.peek()>S.charAt(i) && K>0){
+            stack.pop();
+            K--;
+            }
+            if(stack.isEmpty() && S.charAt(i)=='0'){
+            continue;
+            }
+            else{
+            stack.push(S.charAt(i));
+            }
+        }
+        
+        while(!stack.isEmpty() && K>0){
+            stack.pop();
+            K--;
+        }
+        if(stack.isEmpty()){
+        return "0";
+        }
+        StringBuilder sb=new StringBuilder("");
+        while(!stack.isEmpty()){
+            sb.append(stack.pop());
+        }
+        String str=sb.reverse().toString();
+        return str;
+    }
+}
