6a7afb57a3017f2786e9d86dca2b1685bbd90dda
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-07-18 13:11:13 +0530

    Update Solution.java

--- Stack/Monotonic Stack/String Based Stack Problem/Decode the string/Solution.java
+++ Stack/Monotonic Stack/String Based Stack Problem/Decode the string/Solution.java
@@ -1,1 +1,30 @@
-
+class Solution{
+    static String decodedString(String s){
+        int num=0;
+        Stack<Integer> ns=new Stack<>();
+        Stack<StringBuilder> ss=new Stack<>();
+        StringBuilder ans=new StringBuilder();
+        for(char c:s.toCharArray()){
+            if(Character.isDigit(c)){
+                num=num*10+(c-'0');
+            }
+            else if(c=='['){
+                ss.push(ans);
+                ans=new StringBuilder();
+                ns.push(num);
+                num=0;
+            }
+            else if(c==']'){
+                StringBuilder temp=ans;
+                ans=ss.pop();
+                int count=ns.pop();
+                while(count-->0)
+                    ans.append(temp);
+            }
+            else{
+                ans.append(c);
+            }
+        }
+        return ans.toString();
+    }
+}
