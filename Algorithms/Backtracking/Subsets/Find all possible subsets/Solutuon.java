85b41fbd5e4e485c3973ea4eb41385b20c58255f
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-08-11 13:32:37 +0530

    Create Solutuon.java

---
+++ Algorithms/Backtracking/Subsets/Find all possible subsets/Solutuon.java
@@ -0,0 +1,25 @@
+/*
+
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+        Arrays.sort(nums);
+        buildSubset(nums, ans, curr, 0);
+        return ans;
+
+    }
+
+    public void buildSubset(int nums[], List<List<Integer>> ans, List<Integer> curr, int startIndex) {
+        ans.add(new ArrayList<>(curr));
+        for (int i = startIndex; i < nums.length; i++) {
+            if (i != startIndex && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            curr.add(nums[i]);
+            buildSubset(nums, ans, curr, i + 1);
+            curr.remove(curr.size() - 1);
+
+        }
+    }
+}
