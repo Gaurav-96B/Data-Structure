0ae02a66271c01033ccccb988aafffd438d2dcef
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-10-11 13:08:09 +0530

    Update Solution.java

--- Algorithms/Sliding Window Technique/Max Sum Subarray of size K/Solution.java
+++ Algorithms/Sliding Window Technique/Max Sum Subarray of size K/Solution.java
@@ -1,14 +1,16 @@
 class Solution{
-    static long maximumSumSubarray(int K, ArrayList<Integer> Arr,int n){
-        long sum=0;
-        long max=Long.MIN_VALUE;
-        for(int i=0;i<n;i++){
-            sum=sum+Arr.get(i);
-            if(i>=K-1){
+    static long maximumSumSubarray(int K, ArrayList<Integer> Arr,int N){
+       long sum=0;
+       long max=Long.MIN_VALUE;
+       int startIndex=0;
+       for(int i=0;i<N;i++){
+           sum=sum+Arr.get(i);
+           if(i>=K-1){
                max=Math.max(max,sum);
-               sum=sum-Arr.get(i-(K-1));
-            }
-        }
+               sum=sum-Arr.get(startIndex);
+               startIndex++;
+           }
+       }
        return max;
     }
 }
