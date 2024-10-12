ab123400846f1b85c49e90837c9a8fe9ed7fbfe5
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-10-12 20:03:35 +0530

    Update Solution.java

--- Algorithms/Two Pointers Technique/Triplet Sum in Array/Solution.java
+++ Algorithms/Two Pointers Technique/Triplet Sum in Array/Solution.java
@@ -1,18 +1,18 @@
 class Solution {
     public static boolean find3Numbers(int A[], int n, int X) {
       Arrays.sort(A);
-      for(int i=0;i<n-2;i++){
+      for(int i=0;i<n;i++){
           int start=i+1;
           int end=n-1;
           while(start<end){
               if(A[start]+A[end]+A[i]==X){
                   return true;
               }
-              else if(A[start]+A[end]+A[i]>X){
-                  end--;
+              else if(A[start]+A[end]+A[i]<X){
+                  start++;
               }
               else{
-                  start++;
+                  end--;
               }
           }
       }
