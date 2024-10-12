39a5c7ac6fc390e5c7707ccd3e519d67f1930636
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-10-12 16:19:40 +0530

    Update Solution.java

--- Algorithms/Sliding Window Technique/Longest Subarray with Sum K(With Positive and Negative Number Both)/Solution.java
+++ Algorithms/Sliding Window Technique/Longest Subarray with Sum K(With Positive and Negative Number Both)/Solution.java
@@ -1,18 +1,20 @@
-class Solution{
-    public static int lenOfLongSubarr (int A[], int N, int K) {
-       Map<Integer,Integer>map=new HashMap<>();
-       map.put(0,-1);
-       int sum=0;
-       int maxLen=0;
-       for(int i=0;i<N;i++){
-           sum=sum+A[i];
-           if(map.containsKey(sum-K)==true){
-               maxLen=Math.max(maxLen,i-map.get(sum-K));
-           }
-           if(map.containsKey(sum)==false){
-               map.put(sum,i);
-           }
-       }
-       return maxLen;
+class Solution {
+    public static int lenOfLongSubarr(int A[], int n, int k) {
+      Map<Integer,Integer>map=new HashMap<>();
+      int prefixSum=0;
+      int maxLen=0;
+      for(int i=0;i<n;i++){
+          prefixSum=prefixSum+A[i];
+          if(prefixSum==k){
+              maxLen=i+1;
+          }
+          if(map.containsKey(prefixSum-k)==true){
+              maxLen=Math.max(maxLen,i-map.get(prefixSum-k));
+          }
+          if(map.containsKey(prefixSum)==false){
+              map.put(prefixSum,i);
+          }
+      }
+      return maxLen;
     }
 }
