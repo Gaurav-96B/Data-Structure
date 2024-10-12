bbfb1949ae0a713d77d5a5b9084e8950d0f03e7a
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-10-12 16:39:21 +0530

    Update Solution.java

--- Algorithms/Sliding Window Technique/Longest Subarray with Sum K(With Positive Number Only)/Solution.java
+++ Algorithms/Sliding Window Technique/Longest Subarray with Sum K(With Positive Number Only)/Solution.java
@@ -1,23 +1,20 @@
-class Solution
-{
-    static ArrayList<Integer> subarraySum(int[] arr, int n, int s) 
-    {
-       ArrayList<Integer>ans=new ArrayList<>();
-       int sum=0;
-       int startIndex=0;
-       for(int endIndex=0;endIndex<n;endIndex++){
-           sum=sum+arr[endIndex];
-           while(startIndex<endIndex&&sum>s){
-               sum=sum-arr[startIndex];
-               startIndex++;
-           }
-           if(sum==s){
-               ans.add(startIndex+1);
-               ans.add(endIndex+1);
-               return ans;
-           }
-           
-       }
+class Solution {
+    public static ArrayList<Integer> subarraySum(int[] arr, int n, int s) {
+      ArrayList<Integer>ans=new ArrayList<>();
+      int prefixSum=0;
+      int startIndex=0;
+      for(int endIndex=0;endIndex<n;endIndex++){
+          prefixSum=prefixSum+arr[endIndex];
+          while(startIndex<endIndex&&prefixSum>s){
+              prefixSum=prefixSum-arr[startIndex];
+              startIndex++;
+          }
+          if(prefixSum==s){
+              ans.add(startIndex+1);
+              ans.add(endIndex+1);
+              return ans;
+          }
+      }
        ans.add(-1);
        return ans;
     }
